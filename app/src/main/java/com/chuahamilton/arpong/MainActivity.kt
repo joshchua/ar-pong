ebfa12da69f4148f940c9b03e7b548466c5875ba
R.J. Hamilton <robert.hamilton@meijer.com>
2019-09-09 13:50:15 -0400

    Starting adding contents to support a launcher screen

--- app/src/main/java/com/chuahamilton/arpong/MainActivity.kt
+++ app/src/main/java/com/chuahamilton/arpong/MainActivity.kt
@@ -7,6 +7,7 @@ import com.chuahamilton.arpong.fragments.MainFragment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
