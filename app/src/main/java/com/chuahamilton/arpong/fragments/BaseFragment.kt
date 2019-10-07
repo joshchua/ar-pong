4a589b5bbf8678f5d8daeb2b75ce793bb0746f80
R.J. Hamilton <robert.hamilton@meijer.com>
2019-10-06 23:04:24 -0400

    Added a user flow to the project and began implementing camera permissions

--- app/src/main/java/com/chuahamilton/arpong/fragments/BaseFragment.kt
+++ app/src/main/java/com/chuahamilton/arpong/fragments/BaseFragment.kt
@@ -3,19 +3,42 @@ package com.chuahamilton.arpong.fragments
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.chuahamilton.arpong.R
 import com.chuahamilton.arpong.viewmodels.MainViewModel
+import com.chuahamilton.arpong.MainActivity
+import android.widget.Toast
+import android.content.Context.ACTIVITY_SERVICE
+import android.app.ActivityManager
+import android.app.Activity
+import android.util.Log
+import android.view.*
+import com.chuahamilton.arpong.util.CameraPermissionHelper
+import com.google.ar.core.ArCoreApk
+
 
 class BaseFragment : Fragment() {
+
+    private val TAG = MainActivity::class.java.simpleName
+    private val MIN_OPENGL_VERSION = 3.0
+
+    // Tracks if we have already triggered an installation request.
+    private var install_requested_: Boolean = false
 
     companion object {
         fun newInstance() = BaseFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!checkIsSupportedDeviceOrFinish(activity!!)) {
+            return
+        }
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,13 +49,39 @@ class BaseFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //Glide.with(this).load(R.drawable.scanning_phone).into(scanning_phone_gif)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        // Example of using Glide to show a GIF
+        // Glide.with(this).load(R.drawable.scanning_phone).into(scanning_phone_gif)
+
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun checkIsSupportedDeviceOrFinish(activity: Activity): Boolean {
+        val openGlVersionString =
+            (activity.getSystemService(ACTIVITY_SERVICE) as ActivityManager).deviceConfigurationInfo.glEsVersion
+        if (java.lang.Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later")
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                .show()
+            activity.finish()
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        if (!CameraPermissionHelper.hasCameraPermission(activity!!)) {
+            Toast.makeText(
+                context!!,
+                getString(R.string.camera_permission_toast),
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 
 }
